aab9137166f3c9dd4271c86bc0494284b0dec35d
lindenkillam <77918412+lindenkillam@users.noreply.github.com>
2023-11-07 22:57:54 -0500

    Increase bird speed and stuff

--- MapGeneration/Assets/Scripts/BirdAI.cs
+++ MapGeneration/Assets/Scripts/BirdAI.cs
@@ -1,25 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class BirdAI : MonoBehaviour
 {
     public static List<GameObject> goalList = new List<GameObject>();
-    //NavMeshAgent agent;
-    //Animator anim;
     [SerializeField] int i;
-    const float speed = 2f;
-
+    const float speed = 20f;
+    Vector3 targetDirection;
+    Vector3 newDirection;
     void Start()
     {
-        //agent = GetComponent<NavMeshAgent>();
         i = Random.Range(0, goalList.Count);
-        //agent.SetDestination(goalList[i].transform.position);
-        //anim = this.GetComponent<Animator>();
-        //anim.SetTrigger("isWalking");
+        targetDirection = goalList[i].transform.position - this.transform.position;
+        newDirection = Vector3.RotateTowards(transform.forward, targetDirection, speed, 0.0f);
+        this.transform.rotation = Quaternion.LookRotation(newDirection);
     }
-
 
     void Update()
     {
@@ -29,8 +25,9 @@ public class BirdAI : MonoBehaviour
         if(Vector3.Distance(goalList[i].transform.position, this.transform.position) < 1)
         {
             i = Random.Range(0, goalList.Count);
-            //agent.SetDestination(goalList[i].transform.position);
+            targetDirection = goalList[i].transform.position - this.transform.position;
+            newDirection = Vector3.RotateTowards(transform.forward, targetDirection, speed * Time.deltaTime, 0.0f);
+            this.transform.rotation = Quaternion.LookRotation(newDirection);
         }
-
     }
 }
